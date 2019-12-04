3875b1af9430aa7782a5edd65bb3e909ec6ce31f
Donald Wasserman <djwasserman@gmail.com>
2019-12-04 09:38:41 -0500

    Compile code

--- dist/index.js
+++ dist/index.js
@@ -493,6 +493,7 @@ module.exports = require("os");
 /***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
 
 const core = __webpack_require__(470);
+const fs = __webpack_require__(747);
 const { GitHub } = __webpack_require__(469);
 
 const NEWLINE = " \n ";
